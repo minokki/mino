3b6bd34a86118b2c218fb0541a9bc91cf01916f3
mino <regular2644@gmail.com>
2023-01-02 01:24:29 +0900

    01_Spring_boot

---
+++ Spring_boot/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,55 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Transactional
+public class MemberService {
+
+    /*
+    * 회원가입
+    * */
+    private  final MemberRepository memberRepository;
+
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member){
+        //같은 이름이 있는 중복회원X
+
+        validateDuplicateMember(member); //중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+        .ifPresent(m -> {
+            throw new IllegalStateException(("이미존재하는 회원입니다."));
+        });
+    }
+
+    /*
+    * 전체회원 조회
+    * */
+    public List<Member> findMembers(){
+    return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+
+}
